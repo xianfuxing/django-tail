36eab9b3340967b41ab3883c5922af3ad66df9ea
xianfuxing <xianfuxing@126.com>
2017-06-13 15:51:49 +0800

    # add ECS action to rollback search

--- tail/static/tail/js/search.js
+++ tail/static/tail/js/search.js
@@ -15,6 +15,10 @@ var _filterElement = function(elem) {
 var _searchInput = document.getElementById("search");
 _searchInput.onkeyup = function(e) {
     _filterValue = this.value;
+    if (window.event.keyCode == 27) {
+        _filterValue = '';
+        this.value = '';
+    }
     _logChildNodes = self.logChildNodes;
     for (i=0; i<_logChildNodes.length; i++) {
         _filterElement(_logChildNodes[i])
